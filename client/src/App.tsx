0bcc82f76ff8134ccbbdb6866bf97324a99044da
Alexis <alexisfabian1230@gmail.com>
2025-01-28 22:55:00 -0300

    tailwind 4 install

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
-
+import './index.css'
 function App() {
   const [count, setCount] = useState(0);
 
@@ -18,16 +17,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+        <h1 className="text-blue-500/50 text-3xl font-bold">hola</h1>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
